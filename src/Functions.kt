5453ca8f05fff1b58c18c8ee8a92ecf6b537b3f2
bilaleluneis <bilaleluneis@gmail.com>
2017-11-23 10:35:03 -0500

    initial commit of functions in Kotlin

---
+++ src/Functions.kt
@@ -0,0 +1,13 @@
+/**
+ * @author Bilal El Uneis
+ * @since Nov 23, 2017
+ * bilaleluneis@gmail.com
+ */
+
+// simple function that will return String .. i guess this is like inline functions in C / C++
+fun printHello() = "Hello World!"
+
+fun main(args: Array<String>) {
+    val functionValue = printHello()
+    println(functionValue)
+}
