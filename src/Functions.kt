a828c96009abad4eb58536030354453012497091
bilaleluneis <bilaleluneis@gmail.com>
2017-11-23 13:38:10 -0500

    added more examples of functions with params, default values,etc

--- src/Functions.kt
+++ src/Functions.kt
@@ -7,7 +7,37 @@
 // simple function that will return String .. i guess this is like inline functions in C / C++
 fun printHello() = "Hello World!"
 
+// function syntax, with default values, this allow the creation
+// of scalable and overloaded functions instead of having to create new
+// function each time with new arguments
+
+fun addOnlyPositive(firstValue: Int =0 , secondValue: Int =0) : Int {
+
+    if(firstValue > 0 && secondValue > 0) {
+        return firstValue + secondValue
+    }
+
+    return 0
+
+}
+
+//interesting example I saw in Kotlin talk on YouTube by Venkat Subramaniam
+fun greet(name: String, message: String ="Hi ${name.length}") {
+    println("$message $name")
+}
+
 fun main(args: Array<String>) {
+
     val functionValue = printHello()
     println(functionValue)
+
+    //example of what could be overloaded function in language like Java !
+    addOnlyPositive(firstValue = 1)
+    addOnlyPositive(secondValue = 2)
+    addOnlyPositive(firstValue = 5, secondValue = 3)
+    addOnlyPositive(secondValue = 2, firstValue = 2) //you can change the order of params .. very cool
+    addOnlyPositive()
+    greet(name="Rooster")
+    greet("Kitty") //this is also valid way to call without using name
+
 }
