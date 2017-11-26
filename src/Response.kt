0557f8d015638f472392fe6aa9cd554482538461
bilaleluneis <bilaleluneis@gmail.com>
2017-11-25 19:32:57 -0500

    example of sealed class

---
+++ src/Response.kt
@@ -0,0 +1,33 @@
+/**
+ * @author Bilal El Uneis
+ * @since Nov 25, 2017
+ * bilaleluneis@gmail.com
+ */
+
+/* example of Sealed class in Kotlin
+You can only extend the class inside this file and it is treated as closed
+from outside this file... I learned about this and the example from a Kotlin
+talk on YouTube by Hadi Hariri
+*/
+sealed class Response
+
+// the bellow classes able to extend Response because they are in same file
+// also notice .. you shouldn't be able to extend the bellow classes in this file
+// or the outside because they are not declared open .. so by default they are final !
+
+class Success(val jsonResponse: String) : Response()
+
+class Failure(val errorCode:Int, val errorMessage: String) : Response()
+
+
+// uncomment bellow and see the error...
+
+//class TryingToExtendFinalClass : Success()
+
+// this is nice example of returning either Success or Failure Response
+fun getResponse() : Response {
+
+    return Success(jsonResponse = "{value : 200}")
+    //return Failure(errorCode = 500 , errorMessage = "Internal Server Error!")
+
+}
