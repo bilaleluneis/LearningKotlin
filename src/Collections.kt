94bed37251fcccb3c96f3ef9fd1dc75752f13542
bilaleluneis <bilaleluneis@gmail.com>
2017-12-12 20:46:49 -0500

    few examples of ways to init arrays !

--- src/Collections.kt
+++ src/Collections.kt
@@ -16,17 +16,33 @@ import java.util.*
 
 class ArrayOperations{
 
-    var arrayOfStrings : Array<String>
+    // simple init array with elements
+    var arrayOfStrings : Array<String> = arrayOf("One","Two","Three")
 
-    init{
-        arrayOfStrings = arrayOf("1","2","3")
+    // takes a closure where i is an index
+    val arrayOfInts = Array(5,{i -> i + 1})
+
+    //another way to write the above, as closer is last argument
+    val anotherarrayOfInts = Array(5){ i-> i+1 }
+
+    //lazy is a function that takes another function, again the closure is last argument
+    val lazyArray by lazy { lazyArrayInit() } // you could write your own function inside.
+
+    private fun lazyArrayInit() : Array<Int> {
+        println("Lazy Init called!")
+        return arrayOf(1,2,3,4,5)
     }
+
+
+
 }
 
 fun main(args: Array<String>) {
 
     val array = ArrayOperations()
-    array.arrayOfStrings += "4" // += operator overloaded to add new element to array
-    println("${Arrays.toString(array.arrayOfStrings)}") //[1, 2, 3, 4]
+    array.arrayOfStrings += "Four" // += operator overloaded to add new element to array
+
+    println("${Arrays.toString(array.arrayOfStrings)}")
+    println("${Arrays.toString(array.arrayOfInts)}")
 
 }
