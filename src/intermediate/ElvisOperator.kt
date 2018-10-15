667926f5dec743559f51343eac802ce8e7b8c667
bilaleluneis <bilaleluneis@gmail.com>
2018-10-14 21:58:28 -0400

    translated python class_example_two.py to Kotlin, to show inheritance and composition.

---
+++ src/intermediate/ElvisOperator.kt
@@ -0,0 +1,19 @@
+package intermediate
+
+/**
+ * @author Bilal El Uneis
+ * @since Oct 2018
+ * bilaleluneis@gmail.com
+ */
+
+fun main(args: Array<String>) {
+
+    val test: String? = "Hello"
+
+    /**
+     *  ?: operator basically says, if the expression on the left is null then
+     *  use / return/ execute the expression on the right instead.
+     */
+    test?.let{ println("String is $it") } ?: println("Empty")
+
+}
